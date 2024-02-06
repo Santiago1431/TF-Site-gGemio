0d0b4944ad9ce61f623b30444bbe32f134d73813
Eurico Santiago Climaco Rodrigues <escr@aluno.ifnmg.edu.br>
2024-02-06 18:19:54 +0000

    part1

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import { selectUsuarios } from "./conectbd";
+
+dotenv.config();      
+const app = express();              
+const port = 5000;                  
+
+app.get("/", (req, res) => {       
+  console.log("Rota Principal solicitada");
+  res.sendFile( "/workspaces/TF-Site-gGemio/front/Pagina Inicial.html" );
+});
+app.get("/adm", (req, res) => {
+    console.log (" Pagina de administrador solicitada")
+    res.sendFile("/workspaces/TF-Site-gGemio/front/pagina administrador.html")
+})
+
+app.get("/membros", async (req, res) => {
+    console.log("Rota membros solicitada");
+    try {
+      const usuarios = await selectUsuarios();
+      res.json(usuarios);
+    } catch (error) {
+      res.status(error.status || 500).json({ message: error.message || "Erro!" });
+    }
+  });
+
+app.listen(port, () => {           
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
